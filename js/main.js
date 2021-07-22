cc89b5c6e6a98d2dd656ca8e86e0dba237558d95
Denis Antonov <antonov25041992@yandex.ru>
2021-07-22 16:36:58 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// new Swiper('.swiper-container', {
+// 	loop: true,
+// 	navigation: {
+// 		nextEl: '.arrow',
+// 	},
+// 	breakpoints: {
+// 		320: {
+// 			slidesPerView: 1,
+// 			spaceBetween: 20
+// 		},
+// 		541: {
+// 			slidesPerView: 2,
+// 			spaceBetween: 40
+// 		}
+// 	}
+// });
+
+// const menuButton = document.querySelector('.menu-button');
+// const menu = document.querySelector('.header');
+// menuButton.addEventListener('click', function () {
+// 	menuButton.classList.toggle('menu-button-active');
+// 	menu.classList.toggle('header-active');
+// });
+
+const movieContainer = () => {
+	const app = document.querySelector('.app');
+	app.textContent = 'Hello, world';
+}
+
+movieContainer();
